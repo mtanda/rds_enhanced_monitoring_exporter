7f5c927111fb4f29fe26a76f5fa395ccf394c23a
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2017-12-25 15:43:42 +0900

    change port

--- rds_enhanced_monitoring_exporter.go
+++ rds_enhanced_monitoring_exporter.go
@@ -281,7 +281,7 @@ type config struct {
 
 func main() {
 	var cfg config
-	flag.StringVar(&cfg.listenAddress, "web.listen-address", ":9201", "Address to listen on for web endpoints.")
+	flag.StringVar(&cfg.listenAddress, "web.listen-address", ":9408", "Address to listen on for web endpoints.")
 	flag.StringVar(&cfg.metricsPath, "web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 	flag.StringVar(&cfg.configFile, "config.file", "./rds_enhanced_monitoring_exporter.yml", "Configuration file path.")
 	flag.Parse()
