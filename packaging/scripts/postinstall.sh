f28f6453ec7fd3f34449e5ab00b1e7efa1a6bd4f
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2020-07-27 18:07:08 +0900

    add goreleaser setting

---
+++ packaging/scripts/postinstall.sh
@@ -0,0 +1,3 @@
+systemctl daemon-reload
+systemctl enable rds-enhanced-monitoring-exporter.service
+systemctl restart rds-enhanced-monitoring-exporter.service
---
+++ packaging/scripts/preremove.sh
@@ -0,0 +1,2 @@
+systemctl stop rds-enhanced-monitoring-exporter.service
+systemctl disable rds-enhanced-monitoring-exporter.service
